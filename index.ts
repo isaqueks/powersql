bae9219015848ce45b82d811ed86a16e30a54333
Isaque <iskschluter@gmail.com>
2021-05-25 15:55:59 -0300

    package

--- index.ts
+++ index.ts
@@ -1,34 +1,14 @@
 import PowerSQL from "./src/powerSql";
-import sql from "./src/powerSqlDefaults";
+import SqlDefaults from "./src/powerSqlDefaults";
+import sqlTypes from "./src/sqlTypes";
 import { PowerSQLTable, PowerSQLTableColumn } from "./src/table";
 
-// Select('id', 'name').from('tableName').where(equal('x, 'y').and(group(not_equal('x', param(y)))))
-
-
-/* PowerSQL.CreateTable({
-	name: 'myTable',
-	columns: {
-		id: 'PRIMARY KEY AUTOINCREMENT NOT NULL',
-		name: 'TEXT'
-	}
-}) */
-
-/* PowerSQL.Select('*').From(myTable).whereEquals({
-	id: 1,
-	password: 'password123'
-}) */
-
-let table = new PowerSQLTable('users', [
-    new PowerSQLTableColumn('id', 'INTEGER', ['AUTOINCREMENT', 'PRIMARY KEY']),
-    new PowerSQLTableColumn('name', 'TEXT'),
-    new PowerSQLTableColumn('email', 'TEXT', ['NOT NULL'])
-]);
-
-const demoUser = {
-    name: 'Isaque',
-    email: 'iskschluter@gmail.com'
+const final = {
+    PowerSQL,
+    PowerSQLDefaults: SqlDefaults,
+    PowerSQLTypes: sqlTypes,
+    PowerSQLTable: PowerSQLTable,
+    PowerSQLTableColumn: PowerSQLTableColumn
 }
 
-console.log(
-    PowerSQL(sql.insertInto(table, demoUser))
-);
+export { final as PowerSQL };
