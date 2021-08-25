599f10ee3feedd3c0c4001cfc39c400df2c03162
Isaque <iskschluter@gmail.com>
2021-08-25 11:51:24 -0300

    exports fix

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,6 @@ import PowerSQL from "./src/powerSql";
 import PowerSQLDefaults from "./src/powerSqlDefaults";
 import PowerSQLTypes from "./src/sqlTypes";
 import { PowerSQLTable, PowerSQLTableColumn } from "./src/table";
-export { PowerSQL, PowerSQLDefaults, PowerSQLTypes, PowerSQLTable, PowerSQLTableColumn };
+import PowerSQLStatementTemplate from "./src/powerSqlStatementTemplate";
+import { PowerSQLStatementFactory, PowerSQLStatement } from "./src/powerSqlStatement";
+export { PowerSQL, PowerSQLDefaults, PowerSQLTypes, PowerSQLTable, PowerSQLTableColumn, PowerSQLStatementTemplate, PowerSQLStatement, PowerSQLStatementFactory };
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.PowerSQLTableColumn = exports.PowerSQLTable = exports.PowerSQLTypes = exports.PowerSQLDefaults = exports.PowerSQL = void 0;
+exports.PowerSQLStatementFactory = exports.PowerSQLStatementTemplate = exports.PowerSQLTableColumn = exports.PowerSQLTable = exports.PowerSQLTypes = exports.PowerSQLDefaults = exports.PowerSQL = void 0;
 const powerSql_1 = __importDefault(require("./src/powerSql"));
 exports.PowerSQL = powerSql_1.default;
 const powerSqlDefaults_1 = __importDefault(require("./src/powerSqlDefaults"));
@@ -13,10 +13,7 @@ exports.PowerSQLTypes = sqlTypes_1.default;
 const table_1 = require("./src/table");
 Object.defineProperty(exports, "PowerSQLTable", { enumerable: true, get: function () { return table_1.PowerSQLTable; } });
 Object.defineProperty(exports, "PowerSQLTableColumn", { enumerable: true, get: function () { return table_1.PowerSQLTableColumn; } });
-const power = {
-    PowerSQL: powerSql_1.default,
-    PowerSQLDefaults: powerSqlDefaults_1.default,
-    PowerSQLTypes: sqlTypes_1.default,
-    PowerSQLTable: table_1.PowerSQLTable,
-    PowerSQLTableColumn: table_1.PowerSQLTableColumn
-};
+const powerSqlStatementTemplate_1 = __importDefault(require("./src/powerSqlStatementTemplate"));
+exports.PowerSQLStatementTemplate = powerSqlStatementTemplate_1.default;
+const powerSqlStatement_1 = require("./src/powerSqlStatement");
+Object.defineProperty(exports, "PowerSQLStatementFactory", { enumerable: true, get: function () { return powerSqlStatement_1.PowerSQLStatementFactory; } });
