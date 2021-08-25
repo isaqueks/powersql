e31c7f857bbc7178d56cb670722875b70d865790
Isaque <iskschluter@gmail.com>
2021-08-25 11:39:23 -0300

    exports fix

--- index.ts
+++ index.ts
@@ -3,18 +3,19 @@ import PowerSQLDefaults from "./src/powerSqlDefaults";
 import PowerSQLTypes from "./src/sqlTypes";
 import { PowerSQLTable, PowerSQLTableColumn } from "./src/table";
 
-const power = {
-    PowerSQL,
-    PowerSQLDefaults: PowerSQLDefaults,
-    PowerSQLTypes: PowerSQLTypes,
-    PowerSQLTable: PowerSQLTable,
-    PowerSQLTableColumn: PowerSQLTableColumn
-}
+import PowerSQLStatementTemplate from "./src/powerSqlStatementTemplate";
+import { PowerSQLStatementFactory, PowerSQLStatement } from "./src/powerSqlStatement";
+
 
 export {
     PowerSQL,
     PowerSQLDefaults,
     PowerSQLTypes,
     PowerSQLTable,
-    PowerSQLTableColumn
+    PowerSQLTableColumn,
+
+    PowerSQLStatementTemplate,
+    PowerSQLStatement,
+    PowerSQLStatementFactory
+
 };
